64fe28dfcc1413082e4c149d0fb963baa7018387
Oscar Rovira <cruzrovira@gmail.com>
2021-09-25 17:09:18 -0600

    add filter entities

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Entities;
 
 use CodeIgniter\Entity\Entity;
@@ -7,10 +6,15 @@ use CodeIgniter\Entity\Entity;
 class User extends Entity
 {
     protected $datamap = [];
-    protected $dates   = [
+    protected $dates = [
         'created_at',
         'updated_at',
         'deleted_at',
     ];
-    protected $casts   = [];
+    protected $casts = [];
+
+    protected function setPassword(String $password)
+    {
+        $this->attributes['password'] = password_hash($password, PASSWORD_DEFAULT);
+    }
 }
