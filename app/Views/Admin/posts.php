de14aed6f1fb6ab7a2da2f83c0e3a99fce1991af
Oscar Rovira <cruzrovira@gmail.com>
2021-10-02 21:04:36 -0600

    change extend admin template

--- app/Views/Admin/posts.php
+++ app/Views/Admin/posts.php
@@ -1,30 +1,12 @@
 <?php session()?>
-<?= $this->extend('layout/template')?>
+<?= $this->extend('Admin/layout/template')?>
 
 <?= $this->section('title') ?>
-<?= session('username')?>
+Lista de articulos
 <?= $this->endSection() ?>
 
 <?= $this->section('body')?>
 
-<section class="section">
-  <div class="container">
-    <h1 class="title">Login</h1>
-    <h2 class="subtitle">
-      Ingresa al sistema ahora!
-    </h2>
-
-    <?php if (session('msg.body')):?>
-    <div
-      class="message <?=session('msg.color')?>">
-      <div class="message-body">
-        <?= session('msg.body')?>
-      </div>
-    </div>
-    <?php endif?>
-
-  </div>
-</section>
 
 
 <?= $this->endSection();
